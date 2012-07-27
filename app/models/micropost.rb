bf374cff1c3e982d33c5c9a90bc6038fec3d93b9
tradespring1 <github1@mailinator.com>
2012-07-27 13:21:29 -0700

    changed welcome to tradespring flash and about tradespring search description.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,7 +3,7 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   has_many :comments
 
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { maximum: 600 }
   validates :user_id, presence: true
   
   default_scope order: 'microposts.created_at DESC'
